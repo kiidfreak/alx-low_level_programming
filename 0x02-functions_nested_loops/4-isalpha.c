905c0bfcfa2ba53cb0f94f7837727f7235fda583
Immanuel Maina <59394569+kiidfreak@users.noreply.github.com>
2021-08-31 17:57:36 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <ctype.h>
 
 /**
@@ -9,7 +9,7 @@
  * Return: Always(0).
  */
 
-int test_isalpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
